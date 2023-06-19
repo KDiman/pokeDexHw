2b71293a4527839c1956e6aff7e5f02e52562530
KDiman <kadimanalata@gmail.com>
2023-06-19 22:34:23 +0800

    Updated Hw #6

--- hw-vending-machine/hw-vending-machine.js
+++ hw-vending-machine/hw-vending-machine.js
@@ -1,44 +1,38 @@
-class vendingMachine {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
+class VendingMachine {
+  constructor() {
+    this.snacks = [
+      { name: "Cheese Ring", price: 15 },
+      { name: "Nova", price: 12 },
+      { name: "Moby", price: 5 },
+    ];
+  }
+
+  vend(position, cash) {
+    let snackPriceIndex = this.snacks.map((snacks) => {
+      return snacks.price;
+    });
+
+    let snackPrice = snackPriceIndex[position];
+
+    let change = cash - snackPrice;
+
+    let snackNameIndex = this.snacks.map((snacks) => {
+      return snacks.name;
+    });
+    let snackName = snackNameIndex[position];
+
+    let receipt = { name: snackName, change: change };
+
+    if (position > this.snacks.length) {
+      console.log("Snack not found");
+    } else if (cash < snackPrice) {
+      console.log("Your money is not enough, sorry");
+    } else {
+      console.log(receipt);
+    }
   }
 }
-
-const snack1 = new vendingMachine("Cheese Ring", 15);
-const snack2 = new vendingMachine("Nova", 12);
-const snack3 = new vendingMachine("Moby", 5);
-
-let snacks = [snack1, snack2, snack3];
-
-const snacksIndex = snacks.map((snacks) => {
-  return snacks;
-});
-
-vendingMachine.vend = function (position, cash) {
-  let snackPriceIndex = snacks.map((snacks) => {
-    return snacks.price;
-  });
-
-  let snackPrice = snackPriceIndex[position];
-
-  let change = cash - snackPrice;
-
-  let snackNameIndex = snacks.map((snacks) => {
-    return snacks.name;
-  });
-  let snackName = snackNameIndex[position];
-
-  let receipt = { name: snackName, change: change };
-
-  if (position > snacks.length) {
-    console.log("Snack not found");
-  } else if (cash < snackPrice) {
-    console.log("Your money is not enough, sorry");
-  } else {
-    console.log(receipt);
-  }
-};
+const vendingMachine = new VendingMachine();
 
 vendingMachine.vend(1, 20);
 vendingMachine.vend(0, 20);
